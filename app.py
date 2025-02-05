a6fc5e3a31f29d9dcdc4c1d0aaa412b97fde55fe
Piyush Jain <piyushjain100201@gmail.com>
2025-02-05 18:11:16 +0530

    front end

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")  #decorator
+def index():
+    return render_template("index.html")
+
+
+
+app.run(debug=True, port=5001)
+
+
